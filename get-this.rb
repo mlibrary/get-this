5d3643d5d5a37fca7ae2a35b89dcc3fc8e8dc3c3
Monique Rio <mrio@umich.edu>
2023-05-02 16:33:29 -0400

    updates ruby; updates node; removes monkeypatch

--- get-this.rb
+++ get-this.rb
@@ -7,9 +7,6 @@ require "omniauth"
 require "omniauth_openid_connect"
 require "sinatra/flash"
 require "faraday/follow_redirects"
-#
-# Monkey patch for omniauth_openid_connect -> openid_connect -> webfinger -> httpclient SSL errors
-require_relative "./lib/monkey_httpclient"
 
 Time.zone = "Eastern Time (US & Canada)"
 
