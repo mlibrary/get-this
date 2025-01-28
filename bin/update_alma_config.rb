5a72578aace825ecdcf1c443c00a101eaddea86e
Monique Rio <mrio@umich.edu>
2025-01-28 16:31:53 -0500

    fixes the gha for updating the config
    * adds bundler cach to the gha
    * adds vendor directory to gitignore
    * fixes the alma_api_key secret
    * fixes the script so it doesn't hardcode the app directory

--- bin/update_alma_config.rb
+++ bin/update_alma_config.rb
@@ -3,10 +3,11 @@ require "alma_rest_client"
 require "json"
 require "tempfile"
 
+config_path = File.absolute_path(File.join(__dir__, "../", "/config", "/institution_hours_exceptions.json"))
 alma_client = AlmaRestClient.client
 
 hours_response = alma_client.get("/conf/open-hours")
 
 exit if hours_response.status != 200
 
-File.write("/app/config/institution_hours_exceptions.json", JSON.pretty_generate(hours_response.body))
+File.write(config_path, JSON.pretty_generate(hours_response.body))
